6fa4684f167c8e93f912493a4519e51e241f7fa9
Oscar García-Olalla Olivera <oscar.olalla@xeridia.com>
2017-04-03 14:55:25 +0200

    Update user

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
-/* GET users listing. */
 /**
  * Get all users
  */
@@ -16,7 +15,9 @@ router.get('/', (req, res, next)=> {
     res.status(200).json(users);
   });
 });
-
+/**
+ * Get user by id
+ */
 router.get('/:id', (req, res, next)=> {
   models.User.findById(req.params.id ,{
     attributes:{
@@ -27,7 +28,9 @@ router.get('/:id', (req, res, next)=> {
     res.status(200).json(user);
   });
 });
-
+/**
+ * Add new user.
+ */
 router.post ('/', (req,res,next)=>{
   models.User.create({
     userName: req.body.userName,
@@ -38,8 +41,31 @@ router.post ('/', (req,res,next)=>{
     res.status(200).json(user)
   })
   .catch((error)=>{
-    res.status(400).json(error)
+    res.status(400).end
   })
 });
+/**
+ * Update an user
+ */
+router.put('/:id',(req,res,next)=>{
+  models.User.findById(req.params.id)
+  .then((user)=>{
+    if (user) {
+      user.userName = req.body.userName ? req.body.userName : user.userName;
+      user.password = req.body.password ? req.body.password : user.password;
+      user.email = req.body.email ? req.body.email : user.email;
+      user.save()
+      .then((user)=>{
+        res.status(200).json({msg:"Success"}) 
+      })
+      .catch((error)=>{
+        res.status(400).end()        
+      })
+    }
+    else {
+      res.status(404).end()          
+    }
+  })
+})
 
 module.exports = router;
