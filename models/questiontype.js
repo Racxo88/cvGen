a8e66eada03a01248f72b15c8fd8e22cbf5bc298
Oscar García-Olalla Olivera <oscar.olalla@xeridia.com>
2017-04-12 16:45:42 +0200

    fix in questiontype

--- models/questiontype.js
+++ models/questiontype.js
@@ -2,9 +2,9 @@
 module.exports = function(sequelize, DataTypes) {
   var QuestionType = sequelize.define('QuestionType', {
     name:{type:DataTypes.STRING, allowNull:false},
-    description:{type:DataTypes.STRING, allowNull:false,
+    description:{type:DataTypes.STRING, allowNull:false},
     image:{type:DataTypes.STRING, allowNull:true}
-  }, {
+    },{
     classMethods: {
       associate: function(models) {
         // associations can be defined here
